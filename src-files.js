dd1d9763cabc7935d52ec799a23abbbc3f830548
AmaVic <AmaVic@users.noreply.github.com>
2025-01-25 12:36:10 +0000

    deploy: b9de1a550a6d12137f632542d8df02e596ac6099

--- src-files.js
+++ src-files.js
@@ -1,3 +1,3 @@
-var srcIndex = new Map(JSON.parse('[["vc_viewer",["",[["config",[],["mod.rs"]],["handlers",[],["errors.rs","mod.rs","pages.rs"]],["middleware",[],["compression.rs","mod.rs"]],["templates",[],["mod.rs"]],["utils",[],["mod.rs"]]],["lib.rs"]]]]'));
+var srcIndex = new Map(JSON.parse('[["vc_viewer",["",[["config",[],["env.rs","mod.rs"]],["handlers",[],["errors.rs","mod.rs","pages.rs"]],["middleware",[],["compression.rs","mod.rs"]],["templates",[],["mod.rs"]],["utils",[],["mod.rs"]]],["lib.rs"]]]]'));
 createSrcSidebar();
-//{"start":36,"fragment_lengths":[204]}
+//{"start":36,"fragment_lengths":[213]}
